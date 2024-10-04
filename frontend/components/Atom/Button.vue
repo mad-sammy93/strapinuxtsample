a5269d122a9f2fef754ea9d5128e83ad4cfff0f7
Sambert Rodrigues <sambert@smartshore.nl>
2024-10-04 15:19:10 +0530

    restored files

---
+++ frontend/components/Atom/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLink :to="link" :class="(size) ? `text-${size}` : 'text-6xl'" class="border-2 rounded-md cursor-pointer bg-opacity-80 bg-black-400 px-10 py-3 mt-7 text-white [text-shadow:_0px_1px_10px_rgb(0_0_0_/_80%)]">{{ text }}</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: String,
+  link: String,
+  size: {
+    default: '2xl',
+    type: String as PropType<'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' |'6xl'>
+  }
+})
+</script>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  // ssr: true, 
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo', '@nuxt/image'],
+  plugins: ['~/plugins/getImageUrl.ts'],
+  devServer: {
+    port: 4400
+  },
+  image: {
+    domains: [`${process.env.NUXT_PUBLIC_INTERNAL_API_ENDPOINT}`],
+    quality: 40,
+    format: ['webp'],
+    ipx: {
+      baseURL: process.env.NUXT_PUBLIC_INTERNAL_API_ENDPOINT,
+    }
+  },
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: `${process.env.NUXT_PUBLIC_INTERNAL_API_ENDPOINT}/graphql`
+      }
+    }
+  },
+  nitro: {
+    devProxy: {
+      '/api': 'http://127.0.0.1:4500'
+    }
+  },
+  runtimeConfig: {
+    public: {
+      // Overwritten with ENV variables (on local these are set in docker-compose)
+      apiEndpoint: '/api',
+      // internalApiEndpoint: '',
+      // useS3: false
+    }
+  }
+})
